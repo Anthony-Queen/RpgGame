8f4cdcad10f6b697f12395a3e3580cc7bd83d7d6
Anthony-Queen <189166184+Anthony-Queen@users.noreply.github.com>
2026-03-16 17:34:18 +0100

    Add first dialogue, next need to dynamically spawn enemies

--- Game/main.gd
+++ Game/main.gd
@@ -1,8 +1,10 @@
 extends Node2D
 @onready var player = $Player
+@onready var ExplanationDialogue = load("res://Dialogues/MainExplanation.dialogue")
 
 func _ready():
 	player.combat_started.connect(Globals.show_battle)
+	DialogueManager.show_dialogue_balloon(ExplanationDialogue, "start") 
 	
 	for enemy in get_tree().get_nodes_in_group("enemy"):
 		if enemy.data.is_dead:
